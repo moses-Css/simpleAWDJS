088277f3cb5756de768665d35f2865855fe33f6a
Moses-css <mosesfr2007@gmail.com>
2025-09-24 09:18:52 +0700

    2

--- server.js
+++ server.js
@@ -39,12 +39,14 @@ app.get("/", (req, res) => {
   const cookieDev = req.cookies.detected_device; // cookie override
   const ua = req.get("User-Agent") || "";
 
-  let device;
-  if (["mobile", "tablet", "desktop"].includes(cookieDev)) {
-    device = cookieDev;
-  } else {
-    device = detectDevice(ua);
-  }
+
+  //Cookie override (Matikan kalau misal server trust user agent bukan cookie pertama) kalau gunakan logic dibawah lebih strict
+  // let device;
+  // if (["mobile", "tablet", "desktop"].includes(cookieDev)) {
+  //   device = cookieDev;
+  // } else {
+  //   device = detectDevice(ua);
+  // }
 
   // per-device content
   let pageInfo;
@@ -74,6 +76,25 @@ app.get("/", (req, res) => {
   // render template (EJS)
   res.render("index", { device, ua, page: pageInfo });
 });
+
+
+//warna
+app.get("/", (req, res) => {
+  const ua = req.headers["user-agent"] || "";
+  const device = detectDevice(ua); // "mobile" | "tablet" | "desktop"
+
+  let theme;
+  if (device === "desktop") {
+    theme = { bg: "from-blue-50 to-blue-100", accent: "blue-600" };
+  } else if (device === "tablet") {
+    theme = { bg: "from-pink-50 to-pink-100", accent: "pink-600" };
+  } else {
+    theme = { bg: "from-emerald-50 to-emerald-100", accent: "emerald-100" };
+  }
+
+  res.render("index", { device, ua, page: pageInfo, theme });
+});
+
 
 app.get("/clear-device-cookie", (req, res) => {
   // clears cookie and renders a helpful page
